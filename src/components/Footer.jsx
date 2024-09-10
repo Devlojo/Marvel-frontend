4c4336375de5806f82476d0fe3f2610d02bbee15
Devlojo <jonathan.lacouture@oclock.school>
2024-09-10 10:58:56 +0200

    change footer + remove logo animation on phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,8 +7,9 @@ const Footer = () => {
         <p>
           {" "}
           copyrights © 2024 | Marvel by{" "}
-          <a href="https://github.com/Devlojo"> Jonathan Lacouture </a> | All
-          rights reserved{" "}
+          <a href="https://github.com/Devlojo"> Jonathan Lacouture </a> Made at{" "}
+          <a href="https://www.lereacteur.io/"> Le Reacteur</a> | All rights
+          reserved{" "}
         </p>
       </footer>
     </>
